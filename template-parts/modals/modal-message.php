0ebc7746f713d90c28705cc816d7b36edec26502
p9tno <plahotin_s@mail.ru>
2026-03-13 17:08:15 +0300

    clean up

--- template-parts/modals/modal-message.php
+++ template-parts/modals/modal-message.php
@@ -12,28 +12,7 @@
                     <div class="modal-title">Admission Form</div>
                 </div>
                 <div class="modal-body">
-
                     <?php echo do_shortcode('[wpforms id="' . $form_id . '"]'); ?>
-    
-                    <!-- <form class="form form__grid form_secondary" action="send.php">
-                        <div class="form__row">
-                            <label>Name</label>
-                            <input type="text" placeholder="Name" />
-                        </div>
-                        <div class="form__row">
-                            <label>Name</label>
-                            <input type="text" placeholder="Phone" />
-                        </div>
-                        <div class="form__row form__row_full">
-                            <label>How we can help?</label>
-                            <input type="text" placeholder="How we can help?" />
-                        </div>
-                        <div class="form__row form__row_btn">
-                            <button class="btn" type="submit">Send</button><span>By clicking the button, you accept the
-                                privacy policy and also agree to the processing of your personal data.</span>
-                        </div>
-                    </form> -->
-    
                 </div>
             </div>
         </div>
